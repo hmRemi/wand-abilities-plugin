7d3d3ab75ae4db44e57f09ba41bb11ddc2305b8f
ziue <remig06@hotmail.com>
2022-02-22 12:15:36 +0100

    Added effect to Lightning Wand

--- src/main/java/me/ziue/wandabilities/abilities/LightningWand.java
+++ src/main/java/me/ziue/wandabilities/abilities/LightningWand.java
@@ -2,14 +2,17 @@ package me.ziue.wandabilities.abilities;
 
 import me.ziue.wandabilities.WandAbilities;
 import me.ziue.wandabilities.listeners.Ability;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashSet;
 
@@ -38,9 +41,28 @@ public class LightningWand extends Ability {
             materialHashSet.add(Material.AIR);
             Block block = player.getTargetBlock(materialHashSet, range); // Get the block the player is looking at, must be within 100 blocks
 
-            player.getWorld().strikeLightning(block.getLocation()); // Summon lightning where the player is looking.
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    play(block.getLocation(), FireworkEffect.builder().with(FireworkEffect.Type.BURST).withColor(Color.RED).withColor(Color.ORANGE).build());
+                }
+            }.runTaskLater(plugin, 2L);
 
+            player.getWorld().strikeLightning(block.getLocation()); // Summon lightning where the player is looking.
             plugin.getAbilityManager().playerMessage(player, this.getAbility());
         }
+    }
+
+    public void play(Location location, FireworkEffect effect) {
+        Firework firework = location.getWorld().spawn(location, Firework.class);
+        FireworkMeta fMeta = firework.getFireworkMeta();
+        fMeta.addEffect(effect);
+        firework.setFireworkMeta(fMeta);
+        new BukkitRunnable(){
+
+            public void run() {
+                firework.detonate();
+            }
+        }.runTaskLater(plugin, 1L);
     }
 }
